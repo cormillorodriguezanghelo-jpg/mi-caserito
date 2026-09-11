3c49ee77759344426073912f843f59cf16f637e0
cormillorodriguezanghelo-jpg <cormillorodriguezanghelo@gmail.com>
2026-09-11 03:21:21 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$usuario = "root";
-$password = ""; 
-$base_datos = "caserito";
+$host = "sql307.infinityfree.com";
+$usuario = "if0_42879200";
+$password = "9SxXYVrA8mz"; 
+$base_datos = "if0_42879200_caserito";
 
 $conexion = new mysqli($host, $usuario, $password, $base_datos);
 
